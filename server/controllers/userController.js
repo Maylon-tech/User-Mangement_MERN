673fe0b1bfaa9b4620d1ca10fe710c7689e0a855
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-03-24 17:16:54 +0900

    build route api and seatch controllers functions.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,10 +5,51 @@ export const getStatus = async (req, res) => {
 
     try {
 
-        const total = await User.find()
+        const total = await User.countDocuments()
+        const active = await User.countDocuments({status: "Active"})
+        const inactive = await User.countDocuments({ status: "Inactive" })
+        
+        res.json({ total, active, inactive })
 
         res.json("Hello World  again")
     } catch (error) {
         res.status(500).json({ message: "Error fetch statistics", error: error.message })
+    }
+}
+
+// Search User
+export const searchUser = async (req, res) => {
+
+    try {
+        const query = req.params.query
+        const page = paserInt(req.params.page) || 1
+        const limit = parseInt(req.query.limit) || 10
+        const skip = (page - 1) * limit
+
+        const searchQuery = {
+            $or: [
+                { name: { $regex: query, $options: "i" } },
+                { email: { $regex: query, $options: "i" } },
+                { phone: { $regex: query, $options: "i" } },
+                { status: { $regex: query, $options: "i" } },
+            ],    
+        }
+        const users = await User
+            .find(searchQuery)
+            .sort({ createdAt: -1 })
+            .skip(skip)
+            .limit(limit)
+        
+        
+        res.json({
+            users,
+            currentPage: page,
+            totalPages: Math.ceil(total / limit),
+            totalUsers: total,
+        })
+
+        
+    } catch (error) {
+        res.status(500).json({ message: "Error searching user", error: error.message })
     }
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getStatus } from '../controllers/userController.js'
+import { getStatus, searchUser } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.get("/status", getStatus)
+router.get("/search/:query", searchUser)
 
 export default router
